b30bbd5517bf8a68d0814b9360b785b5c469fab1
Erfan Iravani <erfaniravani00@gmail.com>
2026-04-29 19:44:57 -0400

    Update loop_nested.c

--- micro-benchmarks/lippen-riscv/loop_nested.c
+++ micro-benchmarks/lippen-riscv/loop_nested.c
@@ -83,7 +83,7 @@ static uint64_t leaf_recursive(uint64_t x, int depth) {
 int main(int argc, char **argv) {
     uint64_t iters  = (argc > 1) ? strtoull(argv[1], 0, 0) : 20000000ULL;
     int trials      = (argc > 2) ? atoi(argv[2]) : 7;
-    int depth       = (argc > 3) ? atoi(argv[3]) : 38;
+    int depth       = (argc > 3) ? atoi(argv[3]) : 8;
 
     if (trials < 1) trials = 1;
     if (depth < 0) depth = 0;
